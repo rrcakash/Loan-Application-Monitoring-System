7a30c765b27596fa4fa1654fe82ce279f8742707
Akash kumar <akumar10@Academic.rrc.ca>
2025-03-06 03:28:51 -0600

    Importing the authenticate to the routes

--- src/api/v1/routes/adminRoutes.ts
+++ src/api/v1/routes/adminRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { setUserRole } from "../controllers/adminController";
-
+import authenticate from "../middleware/authenticate";
 
 const router = express.Router();
 
 // Use the authenticateUser middleware
-router.post("/admin/set-role", setUserRole);
+router.post("/admin/set-role",authenticate, setUserRole);
 
 export default router;
--- src/api/v1/routes/userRoutes.ts
+++ src/api/v1/routes/userRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { getUserDetails } from "../controllers/userController";
-
+import authenticate from "../middleware/authenticate";
 
 const router = express.Router();
 
-router.get("/user/:uid", getUserDetails);
+router.get("/user/:uid",authenticate, getUserDetails);
 
 export default router;
