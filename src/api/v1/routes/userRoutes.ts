e881396ab2cadbd23aed8e4cebeafdcd17d2c63e
Akash kumar <akumar10@Academic.rrc.ca>
2025-03-06 05:22:51 -0600

    Assigning the roles for the user routes

--- src/api/v1/routes/userRoutes.ts
+++ src/api/v1/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { getUserDetails } from "../controllers/userController";
 import authenticate from "../middleware/authenticate";
+import isAuthorized from "../middleware/authorize"; // Import the authorization middleware
 
 const router = express.Router();
 
-router.get("/user/:uid",authenticate, getUserDetails);
+router.get("/user/:uid",authenticate,isAuthorized({ hasRole: ["user"] }), getUserDetails);
 
 export default router;
