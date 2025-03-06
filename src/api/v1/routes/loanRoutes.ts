c32db2a50358bcdab0a2cc0ba1a6cbc4e09a4963
Akash kumar <akumar10@Academic.rrc.ca>
2025-03-06 03:35:32 -0600

    adding authentication in the loan routes

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { getLoans, createLoan, updateLoanStatus } from '../controllers/loanController';
+import authenticate from '../middleware/authenticate'; 
+import { auth } from 'src/config/firebase';
 
 const router = Router();
 
 // Define the API routes
-router.post('/loans', createLoan);         // Role: user
-router.put('/loans/:id/review', updateLoanStatus);   // Role: officer
-router.get('/loans', getLoans);            // Role: officer
-router.put('/loans/:id/approve', updateLoanStatus);  // Role: manager
+router.post('/loans',authenticate, createLoan);         // Role: user
+router.put('/loans/:id/review',authenticate, updateLoanStatus);   // Role: officer
+router.get('/loans',authenticate, getLoans);            // Role: officer
+router.put('/loans/:id/approve',authenticate, updateLoanStatus);  // Role: manager
 
 export default router;
